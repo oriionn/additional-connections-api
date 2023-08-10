8b5fa6c1ec86a1efda19f4128e3fa8d406530230
oriionn <xemilien67@gmail.com>
2023-08-10 03:55:17 +0200

    Add cors

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const cors = require('cors')
 const app = express();
 app.use(express.json());
+app.use(cors({ origin: "*" }))
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 
